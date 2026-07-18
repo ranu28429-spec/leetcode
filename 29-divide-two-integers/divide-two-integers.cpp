f0e924dc40ba92701b44ce5a616752ca528c8cf7
Ranu Yadav <ranu28429@gmail.com>
2026-07-18 20:45:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (39.51%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.cpp
+++ 29-divide-two-integers/divide-two-integers.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        double ans = 0;
         if(dividend==INT_MIN && divisor==-1)
         return INT_MAX;
           if(dividend==INT_MIN && divisor==1)
         return INT_MIN;
-        ans = 1LL*dividend / divisor;
-        return ans;
+        dividend = 1LL*dividend / divisor;
+        return dividend;
     }
 };
