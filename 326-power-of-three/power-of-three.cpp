48b785e6fdf2e79e67240654616c5da17b8c11a9
Ranu Yadav <ranu28429@gmail.com>
2026-08-06 22:31:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (98.03%) - LeetSync

---
+++ 326-power-of-three/power-of-three.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n==1)
+        return 1;
+
+        else if(n<3)
+        return 0;
+       
+        while(n!=1){
+            if(n%3!=0)
+            return 0;
+            n=n/3;
+        }
+            return 1;
+        }
+        
+    };
